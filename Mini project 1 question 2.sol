356f04cea21efd8a56a5dc597f86a3581862cc7c
MoMih2022 <114233598+MoMih2022@users.noreply.github.com>
2022-09-30 23:06:09 +0100

    Add files via upload

---
+++ Mini project 1 question 2.sol
@@ -0,0 +1,12 @@
+/* Give 2 examples of the following: fixed array and dynamic array.*/
+// SPDX-License-Identifier: MIT
+pragma solidity ^ 0.8.7;
+contract Momo {
+    // Fixed array
+    uint [3] public Momoo = [2,3,4];
+    uint [10] public Moomoo = [0,1,2,3,4,5,6,7,8,9];
+
+    // Dynamic array
+    uint [] public Halimat = [8,6,4 ];
+    string [] public Michael = ["M","i","c","h","a","e","l"];
+}
